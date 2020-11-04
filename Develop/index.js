5a002363e713bb9ae1d38a44a1bf034dab05c63a
tedkkraft <tedkkraft@gmail.com>
2020-11-03 20:04:41 -0500

    Initial commit, questions added

---
+++ Develop/index.js
@@ -0,0 +1,48 @@
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },{
+        type: "input",
+        name: "description",
+        message: "Give a quick description of your project."
+    },{
+        type: "input",
+        name: "installation",
+        message: "What is required to install your project?"
+    },{
+        type: "input",
+        name: "usage",
+        message: "What is the primary function of your application?"
+    },{
+        type: "input",
+        name: "license",
+        message: "What license would you like on your project?"
+    },{
+        type: "input",
+        name: "contributors",
+        message: "Who contributed to this project?"
+    },{
+        type: "input",
+        name: "tests",
+        message: "How can users test your application?"
+    },{
+        type: "input",
+        name: "questions",
+        message: "Where can users direct questions about this project?"
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
