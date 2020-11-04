99f9120e27dc7aaae3ae6350f3a719d84b85094f
tedkkraft <tedkkraft@gmail.com>
2020-11-03 20:48:33 -0500

    generateMarkdown function built

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,40 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Installation
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributors
+
+${data.contributors}
+
+## Tests
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about this project, please direct them here:
+
+${data.questions}
+
 `;
 }
 
